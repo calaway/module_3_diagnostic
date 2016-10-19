474815850ed0f936fb1d701415a75d3e582e778d
Calaway <Christopher.Calaway@gmail.com>
2016-10-19 09:42:18 -0600

    Look up documentation. Working on Search controller and working with postman to get content from api.

--- spec/features/user_can_search_for_stations_spec.rb
+++ spec/features/user_can_search_for_stations_spec.rb
@@ -10,8 +10,10 @@ RSpec.feature "User can search for stations" do
     # And I click "Locate"
     click_on 'Locate'
 # Then I should be on page "/search" with parameters visible in the url
+    # expect(page).to have_current_path(search_path(q: '80203'))
     expect(current_path).to eq('/search')
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    expect(page).to have_content('something')
     # And the stations should be limited to Electric and Propane
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
   end
